b14d2248a06e70f48115acbd6ea596cd9162ca5a
WataganDental <team@watagandental.com.au>
2025-06-29 01:19:18 +1000

    Complete Tailwind CSS v4 and DaisyUI v5 upgrade

--- public/js/modules/notifications.js
+++ public/js/modules/notifications.js
@@ -50,14 +50,14 @@ export class NotificationManager {
         notification.innerHTML = `
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
-                    <div class="flex-shrink-0">
+                    <div class="shrink-0">
                         ${this.getIcon(type)}
                     </div>
                     <div class="ml-3">
                         <p class="text-sm font-medium">${message}</p>
                     </div>
                 </div>
-                <button class="ml-4 flex-shrink-0 text-gray-400 hover:text-gray-600" onclick="this.parentElement.parentElement.remove()">
+                <button class="ml-4 shrink-0 text-gray-400 hover:text-gray-600" onclick="this.parentElement.parentElement.remove()">
                     <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                     </svg>
